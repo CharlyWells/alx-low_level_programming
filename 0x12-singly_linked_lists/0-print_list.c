ec9a22ea2c94c5b023c0e005eec5a7e8283fbff0
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-10-19 14:52:19 -0700

    Added new files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+    size_t node_count = 0;
+
+    while (h != NULL) {
+        if (h->str != NULL) {
+            printf("[%u] %s\n", h->len, h->str);
+        } else {
+            printf("[0] (nil)\n");
+        }
+        h = h->next;
+        node_count++;
+    }
+
+    return node_count;
+}
