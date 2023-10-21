429452d1240a4e08c3104078afe7ad3f2ef4d1c0
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-10-21 16:30:16 -0700

    Added new files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,8 @@
 #include "main.h"
 
-/**
- * print_alphabet - print all alphabet in lowercase
- * is written by charlotte
- */
-
-void print_alphabet(void)
-{
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-	_putchar('\n');
+void print_alphabet(void){
+    char letter;
+    for(letter='a'; letter<='z'; letter++)
+      putchar(letter);
+      putchar('\n');
 }
