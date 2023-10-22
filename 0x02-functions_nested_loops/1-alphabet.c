38ef4f5b118ce68cf43cc5a9f466f5f9f6a57a9e
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-10-21 17:12:54 -0700

    Added new files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,15 @@
 
 /**
  * print_alphabet - print all alphabet in lowercase
+ * written by charlotte
  */
 
 void print_alphabet(void)
 {
     char letter;
+
     for(letter = 'a'; letter <= 'z'; letter++)
       _putchar(letter);
+
      _putchar('\n');
 }
