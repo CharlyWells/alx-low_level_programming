4de5a10476af8c9012be7c15bee03c81899d9027
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-10-22 22:59:34 -0700

    Added new files

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,6 +6,10 @@
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c){
- {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+ if(c == 'c' && c == 'C'){
+    return (1);
+ }
+   else{
+    return (0)
+   }
 }
